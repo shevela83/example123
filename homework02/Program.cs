7f7d3edd8da2d2c520df848cc189a572abc8f982
Shevela_Denis <shevela83@mail.ru>
2023-02-13 11:43:04 +0300

    homework02

---
+++ homework02/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+if (number % 2 == 0)
+    Console.WriteLine("Четное: ");
+else Console.WriteLine("Нечетное: ");
