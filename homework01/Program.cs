7fbc682a898cac6ebb109f3efe61809c401b098d
Shevela_Denis <shevela83@mail.ru>
2023-02-13 11:26:43 +0300

    Homework1

---
+++ homework01/Program.cs
@@ -0,0 +1,19 @@
+﻿//три числа на ввод max
+Console.Write ("Введите первое число: ");
+int first = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int second = int.Parse(Console.ReadLine() ?? "0");
+Console.Write ("Введите третье число: ");
+int third = int.Parse(Console.ReadLine() ?? "0");
+int max = first;
+if(second > first)
+{
+    second = max;
+}
+if(third > max)
+{
+    max = third;
+}
+{
+    Console.WriteLine ("Число " + max);
+}
